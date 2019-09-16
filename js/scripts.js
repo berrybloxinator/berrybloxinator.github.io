0ebee20651859bc45a4df8d447105657078b3804
Brady Liechty <berrybloxinator@gmail.com>
2019-09-15 22:22:02 -0600

    Update

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function firstPar() {
+    let e = document.getElementById('first-par');
+
+    if (e.style.display === 'none') {
+        e.style.display = 'block';
+    } else {
+        e.style.display = 'none';
+    }
+}
+
+function secPar() {
+    let e = document.getElementById('second-par');
+
+    if (e.style.display === 'none') {
+        e.style.display = 'block';
+    } else {
+        e.style.display = 'none';
+    }
+}
